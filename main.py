26c447dfe8437a04d09138c50c7281e7e8ae5d08
Alisher <mcmuffin620@gmail.com>
2021-03-01 18:26:13 +0100

    added movement / time played feature / restart feature | need to fix bugs tmrw

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter
+import time
 
-canvas = tkinter.Canvas(width=400, height=400)
+canvas_window_size = 400
+
+canvas = tkinter.Canvas(width=canvas_window_size, height=canvas_window_size)
 canvas.configure(background='grey')
 canvas.pack()
 
@@ -9,39 +12,67 @@ def custom_init():
     global coords 
     global snake_head
     global score
+    global start_time
 
-    coords = [190, 190, 210, 210]
+    start_time = time.time()
+    score = 0
+    time_played = 0
+    coords = [180, 180, 200, 200]
     snake_head = canvas.create_rectangle(coords[0], coords[1], coords[2], coords[3], fill='black') #vytvorim zaciatocny bod
-    snake_movement_north()
+    snake_movement_north() #zacnem pohyb
 
 #----game over----
 def game_over():
-    print('game over')
-    print(f'skore:', score)
-    print(f'time_played:', time_played)
+    time_played = round(time.time()-start_time, 2)  #time_played v sekundach
+    print('koniec hry')
+    print(f'skore: {score} bodov')
+    print(f'odohrany cas: {time_played}s')
+
+#----restart----
+def restart(event):
+    canvas.create_rectangle(coords[0], coords[1], coords[2], coords[3], fill='grey', outline='grey')
+    canvas.configure(background='grey')
+    custom_init()
 
 #----movement----
 def snake_movement_north():
-    if coords[1]-20 == 0 and coords[3]-20 == 0:
+    if coords[1]-20 <= 0:
         canvas.configure(background='red')
         game_over()
     else:
         coords[1] -= 20
         coords[3] -= 20
         canvas.coords(snake_head, coords[0], coords[1], coords[2], coords[3])
-        canvas.after(200, snake_movement_north)
+        canvas.after(300, snake_movement_north)
+
+def snake_movement_south():
+    pass
 
 def snake_movement_east():
-    if coords[1]-20 == 0 and coords[3]-20 == 0:
+    if coords[2] >= canvas_window_size:
         canvas.configure(background='red')
         game_over()
     else:
-        coords[1] -= 20
-        coords[3] -= 20
+        coords[0] += 20
+        coords[2] += 20
+        print(coords)
         canvas.coords(snake_head, coords[0], coords[1], coords[2], coords[3])
         canvas.after(200, snake_movement_east)
 
+def snake_movement_west():
+    pass
 
+
+#----controls----
+#canvas.bind('<Up>', snake_movement_north)
+#canvas.bind('<Down>', snake_movement_south)
+#canvas.bind('<Right>', snake_movement_east)
+#anvas.bind('<Left>', snake_movement_west)
+#canvas.bind_all('w', snake_movement_north)
+#canvas.bind_all('s', snake_movement_south)
+#canvas.bind_all('d', snake_movement_east)
+#canvas.bind_all('a', snake_movement_west)
+#canvas.bind_all("r", restart)
 
 
 custom_init()
